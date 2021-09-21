c76cbf61a89b51446f775a18fea6fd36b57e11de
Tim Bikbaev <tbikb@DESKTOP-A7C8FAP.localdomain>
2021-09-21 18:01:25 +0300

    cv api init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let contacts = warp::path!("contacts" / String)
+        .map(|source: String| 
+            match source.as_str() {
+                "mail" => "t.bikbaev@gmail.com",
+                "skype" => "tim.bikbaev",
+                "phone" => "+380671014882",
+                "telegram" => "@t_bikbaev",
+                _ => "Not found"
+            });
+
+    warp::serve(contacts)
+        .run(([0, 0, 0, 0], 5000))
+        .await;
+}
