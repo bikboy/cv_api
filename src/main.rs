40b27c67ca7f606916f786e9c68bc0bdb850c728
Tim Bikbaev <tbikb@DESKTOP-A7C8FAP.localdomain>
2021-10-17 23:15:44 +0300

    summary, experience , education sections added

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,29 @@ use std::io::Read;
 async fn main() {
     let mut contacts_file = File::open("contacts.json").unwrap();
     let mut skills_file = File::open("skillsv2.json").unwrap();
+    let mut summary_file = File::open("summary.json").unwrap();
+    let mut experience_file = File::open("experience.json").unwrap();
+    let mut education_file = File::open("education.json").unwrap();
     let mut contacts_data = String::new();
     let mut skills_data = String::new();
+    let mut summary_data = String::new();
+    let mut experience_data = String::new();
+    let mut education_data = String::new();
     contacts_file.read_to_string(&mut contacts_data).unwrap();
     skills_file.read_to_string(&mut skills_data).unwrap();
+    summary_file.read_to_string(&mut summary_data).unwrap();
+    experience_file.read_to_string(&mut experience_data).unwrap();
+    education_file.read_to_string(&mut education_data).unwrap();
     let contacts_json: serde_json::Value =
         serde_json::from_str(&contacts_data).expect("JSON was not well-formatted");
     let skills_json: serde_json::Value =
         serde_json::from_str(&skills_data).expect("JSON was not well-formatted");
+    let summary_json: serde_json::Value =
+        serde_json::from_str(&summary_data).expect("JSON was not well-formatted");
+    let experience_json: serde_json::Value =
+        serde_json::from_str(&experience_data).expect("JSON was not well-formatted");
+    let education_json: serde_json::Value =
+        serde_json::from_str(&education_data).expect("JSON was not well-formatted");
     let err: serde_json::Value =
         serde_json::json!({"error": "API path not found"});
     let routes = warp::path!( "api" / String)
@@ -22,6 +37,9 @@ async fn main() {
             match source.as_str() {
                 "contacts" => Ok(warp::reply::json(&contacts_json)),
                 "skills" => Ok(warp::reply::json(&skills_json)),
+                "summary" => Ok(warp::reply::json(&summary_json)),
+                "experience" => Ok(warp::reply::json(&experience_json)),
+                "education" => Ok(warp::reply::json(&education_json)),
                 _ => Ok(warp::reply::json(&err))
             });
     let cors = warp::cors()
